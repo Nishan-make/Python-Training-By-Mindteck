c51062ca360ea1eed5070c9ad067c376a27e943e
Nishan mal <64873305+Nishan-make@users.noreply.github.com>
2025-08-30 08:41:05 +0530

    Update assignment2_29_08_25.py

--- Assignments/assignment2_29_08_25.py
+++ Assignments/assignment2_29_08_25.py
@@ -1,12 +1,34 @@
-marks = 90
+marks = int(input("Please enter your Mark - "))
+#Using if-elif-else
+Grade = ""
 if marks >= 90 :
-    print("Grade A")
+    Grade = "A"
 elif marks >= 80:
-    print("Grade B")
+    Grade = "B"
 elif marks >= 70:
-    print("Grade C")
+    Grade = "C"
 elif marks >= 60:
-    print("Grade D")
+    Grade = "D"
 else:
-    print("Grade F")
+    Grade = "F"
+print("After Using if-else, Grade - ", Grade)
+
+#Using Nested if-else
+Grade = ""
+if marks >= 90 :
+    Grade = "A"
+else:
+    if marks >= 80:
+        Grade = "B"
+    else:
+        if marks >= 70:
+            Grade = "C"
+        else:
+            if marks >= 60:
+                Grade = "D"
+            else:
+                Grade = "F"
+print("After Using Using nested if-else, Grade - ", Grade)
+
+
 
